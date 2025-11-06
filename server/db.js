f5771473f07ed0d27a789aa5cce1de390258fc2a
Afsal <muhammedafsalthayyil@gmail.com>
2025-11-06 00:04:13 -0500

    added MongoDB database

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:DzKQwLVji32mVt6nHMDhfVEb@172.21.131.205:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
